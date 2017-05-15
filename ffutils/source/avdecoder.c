2bd6c7f8369e4fcdfdb1ab7aba30d660c1aa97b7
ireader <ch.magus@163.com>
2017-05-15 18:11:02 +0800

    ffmpeg audio format convert

--- ffutils/source/avdecoder.c
+++ ffutils/source/avdecoder.c
@@ -46,44 +46,78 @@ static void avpacket_to(const struct avpacket_t* avpkt, AVPacket* pkt)
 	pkt->flags = avpkt->flags;
 }
 
-static void avframe_to(const struct avframe_t* avframe, AVFrame* frame)
+static int ffmpeg_audio_format_from(int format)
 {
-	int i;
-	memset(frame, 0, sizeof(AVFrame));
-	frame->format = avframe->format;
-	frame->pkt_pts = avframe->pts;
-	frame->pkt_dts = avframe->dts;
-	frame->flags = avframe->flags;
-	frame->width = avframe->width;
-	frame->height = avframe->height;
-	frame->channels = avframe->channels;
-	frame->nb_samples = avframe->samples;
-	frame->sample_rate = avframe->sample_rate;
-	for (i = 0; i < sizeof(avframe->data) / sizeof(avframe->data[0]); i++)
+	switch (format)
 	{
-		frame->data[i] = avframe->data[i];
-		frame->linesize[i] = avframe->linesize[i];
+	case PCM_SAMPLE_FMT_U8: return AV_SAMPLE_FMT_U8;
+	case PCM_SAMPLE_FMT_S16: return AV_SAMPLE_FMT_S16;
+	case PCM_SAMPLE_FMT_S32: return AV_SAMPLE_FMT_S32;
+	case PCM_SAMPLE_FMT_FLOAT: return AV_SAMPLE_FMT_FLT;
+	case PCM_SAMPLE_FMT_DOUBLE: return AV_SAMPLE_FMT_DBL;
+	case PCM_SAMPLE_FMT_S16P: return AV_SAMPLE_FMT_S16P;
+	case PCM_SAMPLE_FMT_S32P: return AV_SAMPLE_FMT_S32P;
+	case PCM_SAMPLE_FMT_FLTP: return AV_SAMPLE_FMT_FLTP;
+	case PCM_SAMPLE_FMT_DBLP: return AV_SAMPLE_FMT_DBLP;
+	default: return AV_SAMPLE_FMT_S16;
 	}
 }
 
-static void avframe_from(const AVFrame* frame, struct avframe_t* avframe)
+static int ffmpeg_audio_format_to(int ffmpeg)
+{
+	switch (ffmpeg)
+	{
+	case AV_SAMPLE_FMT_U8: return PCM_SAMPLE_FMT_U8;
+	case AV_SAMPLE_FMT_S16: return PCM_SAMPLE_FMT_S16;
+	case AV_SAMPLE_FMT_S32: return PCM_SAMPLE_FMT_S32;
+	case AV_SAMPLE_FMT_FLT: return PCM_SAMPLE_FMT_FLOAT;
+	case AV_SAMPLE_FMT_DBL: return PCM_SAMPLE_FMT_DOUBLE;
+	case AV_SAMPLE_FMT_S16P: return PCM_SAMPLE_FMT_S16P;
+	case AV_SAMPLE_FMT_S32P: return PCM_SAMPLE_FMT_S32P;
+	case AV_SAMPLE_FMT_FLTP: return PCM_SAMPLE_FMT_FLTP;
+	case AV_SAMPLE_FMT_DBLP: return PCM_SAMPLE_FMT_DBLP;
+	default: return PCM_SAMPLE_FMT_S16;
+	}
+}
+
+static void avframe_to(const struct avframe_t* avframe, AVFrame* ffmpeg)
+{
+	int i;
+	memset(ffmpeg, 0, sizeof(AVFrame));
+	ffmpeg->format = avframe->samples > 0 ? ffmpeg_audio_format_from(avframe->format) : avframe->format;
+	ffmpeg->pkt_pts = avframe->pts;
+	ffmpeg->pkt_dts = avframe->dts;
+	ffmpeg->flags = avframe->flags;
+	ffmpeg->width = avframe->width;
+	ffmpeg->height = avframe->height;
+	ffmpeg->channels = avframe->channels;
+	ffmpeg->nb_samples = avframe->samples;
+	ffmpeg->sample_rate = avframe->sample_rate;
+	for (i = 0; i < sizeof(avframe->data) / sizeof(avframe->data[0]); i++)
+	{
+		ffmpeg->data[i] = avframe->data[i];
+		ffmpeg->linesize[i] = avframe->linesize[i];
+	}
+}
+
+static void avframe_from(const AVFrame* ffmpeg, struct avframe_t* avframe)
 {
 	int i;
 	memset(avframe, 0, sizeof(struct avframe_t));
-	avframe->format = frame->format; //AV_PIX_FMT_YUV420P;
-	avframe->pts = frame->pkt_pts;
-	avframe->dts = frame->pkt_dts;
-	avframe->flags = frame->flags;
-	avframe->width = frame->width;
-	avframe->height = frame->height;
-	avframe->channels = frame->channels;
-	avframe->samples = frame->nb_samples;
-	avframe->sample_bits = 8 * av_get_bytes_per_sample(frame->format);
-	avframe->sample_rate = frame->sample_rate;
+	avframe->format = ffmpeg->nb_samples > 0 ? ffmpeg_audio_format_to(ffmpeg->format) : ffmpeg->format; //AV_PIX_FMT_YUV420P;
+	avframe->pts = ffmpeg->pkt_pts;
+	avframe->dts = ffmpeg->pkt_dts;
+	avframe->flags = ffmpeg->flags;
+	avframe->width = ffmpeg->width;
+	avframe->height = ffmpeg->height;
+	avframe->channels = ffmpeg->channels;
+	avframe->samples = ffmpeg->nb_samples;
+	avframe->sample_bits = 8 * av_get_bytes_per_sample(ffmpeg->format);
+	avframe->sample_rate = ffmpeg->sample_rate;
 	for (i = 0; i < sizeof(avframe->data)/sizeof(avframe->data[0]); i++)
 	{
-		avframe->data[i] = frame->data[i];
-		avframe->linesize[i] = frame->linesize[i];
+		avframe->data[i] = ffmpeg->data[i];
+		avframe->linesize[i] = ffmpeg->linesize[i];
 	}
 }
 
@@ -91,9 +125,9 @@ static void avframe_from(const AVFrame* frame, struct avframe_t* avframe)
 int avdecoder_input(void* ff, const struct avpacket_t* pkt)
 {
 	AVPacket pkt2;
-	if(NULL != pkt)
+	if (NULL != pkt)
 		avpacket_to(pkt, &pkt2);
-	return ffdecoder_input(ff, NULL==pkt ? NULL : &pkt2);
+	return ffdecoder_input(ff, NULL == pkt ? NULL : &pkt2);
 }
 
 /// @param[in] frame must be memset to 0 or from av_frame_alloc()
