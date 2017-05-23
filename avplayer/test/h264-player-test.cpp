581d527497ae04c05712a57d161aaeea4ba1cd86
ireader <ch.magus@163.com>
2017-05-23 10:07:44 +0800

    h264 file player

---
+++ avplayer/test/h264-player-test.cpp
@@ -0,0 +1,102 @@
+#include "avplayer-file.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+static uint8_t s_buffer[10 * 1024 * 1024];
+
+static inline const uint8_t* h264_startcode(const uint8_t *data, ptrdiff_t bytes)
+{
+	ptrdiff_t i;
+	for (i = 2; i + 1 < bytes; i++)
+	{
+		if (0x01 == data[i] && 0x00 == data[i - 1] && 0x00 == data[i - 2])
+		{
+			if (i >= 3 && 0x00 == data[i - 3])
+				return data + (i - 3);
+			else
+				return data + (i - 2);
+		}
+	}
+
+	return NULL;
+}
+
+static inline const uint8_t* h264_frame(const uint8_t *data, ptrdiff_t bytes)
+{
+	uint8_t naltype;
+	const uint8_t *p;
+
+	while (bytes > 4)
+	{
+		p = h264_startcode(data, bytes);
+		if (!p)
+			return data + bytes;
+
+		naltype = p[0x01 == p[2] ? 3 : 4] & 0x1f;
+		// 1: no-IDR slice
+		// 2: A-slice
+		// 3: B-slice
+		// 4: C-slice
+		// 5: IDR frame
+		if (naltype > 0 && naltype < 6)
+		{
+			return p;
+		}
+
+		p += 3;
+		bytes -= p - data;
+		data = p;
+	}
+
+	return data + bytes;
+}
+
+struct avpacket_t* h264_packet(uint8_t* data, size_t bytes, int64_t pts, int64_t dts)
+{
+	struct avpacket_t* pkt;
+	pkt = (struct avpacket_t*)malloc(sizeof(*pkt));
+	memset(pkt, 0, sizeof(struct avpacket_t));
+	pkt->data = (uint8_t*)(pkt + 1);
+	pkt->bytes = bytes;
+	pkt->pts = pts;
+	pkt->dts = dts;
+	pkt->data = data;
+	return pkt;
+}
+
+static int h264_file_reader(void* end, struct avpacket_t** pkt, int* type)
+{
+	static int64_t pts = 0;
+	static const uint8_t* p = NULL;
+
+	if (NULL == p)
+	{
+		p = h264_startcode(s_buffer, (uint8_t*)end - s_buffer);
+	}
+
+	if (p >= end)
+		return 0;
+
+	const uint8_t* next = h264_frame(p, (uint8_t*)end - p);
+	next = h264_startcode(next + 4, (uint8_t*)end - next - 4);
+
+	*pkt = h264_packet((uint8_t*)p, next - p, pts, pts);
+	*type = 1;
+
+	p = next;
+	pts += 40; // 40ms
+	return 1;
+}
+
+int h264_player_test(void* window, const char* h264)
+{
+	FILE* fp = fopen(h264, "rb");
+	int r = fread(s_buffer, 1, sizeof(s_buffer), fp);
+	fclose(fp);
+
+	uint8_t *end = s_buffer + r;
+	void* player = avplayer_file_create(window, h264_file_reader, end);
+	avplayer_file_play(player);
+	return 0;
+}
