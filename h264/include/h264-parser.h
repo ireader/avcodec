c5a1570260ee64fe964f6cadae804b5ef908bd45
alexliyu7352 <liyu7352@gmail.com>
2022-04-06 13:51:05 +0800

    编译问题

--- h264/include/h264-parser.h
+++ h264/include/h264-parser.h
@@ -2,7 +2,9 @@
 #define _h264_parser_h_
 
 #include <stddef.h>
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 struct h264_parser_t;
 struct h264_parser_t* h264_parser_create(void);
 void h264_parser_destroy(struct h264_parser_t* parser);
@@ -12,5 +14,8 @@ int h264_parser_input(struct h264_parser_t* parser, const void* annexb, size_t b
 int h264_parser_getflags(struct h264_parser_t* parser);
 
 int h264_parser_iskeyframe(struct h264_parser_t* parser);
-
+#ifdef __cplusplus
+}
+#endif
 #endif /* !_h264_parser_h_ */
+
